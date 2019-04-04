93356747fd70881f1d155e9b526ce0f891977edb
Pascual <pascual.munoz@pccomponentes.com>
2019-04-05 01:10:18 +0200

    bendito rxjs

--- src/components/Users/index.js
+++ src/components/Users/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import GenericTable from '../GenericTable'
+import {ajax} from 'rxjs/ajax'
+console.log('ajax', ajax);//TODO: borrame.
 
 export default class Users extends React.Component {
     constructor(props) {
@@ -11,6 +13,20 @@ export default class Users extends React.Component {
         };
     }
     componentDidMount() {
+        //creamos un observable del json que tira el endpoint,
+        // de ahi el dolar del final
+        // el flujo es de [ 1json ] al igual que el de un array de [1] elemento.
+        const usersJson$ = ajax.getJSON('http://localhost:6969/users')
+
+        //nos subscribimos a ese flujo como observadores, para ello , le pasamos
+        //3 callbacks ...
+        usersJson$.subscribe(
+            console.log, //onSuccess,
+            console.error, //onError
+            console.log('completado') //onComplete , que este se usa poco xD
+        )
+        //y asincronia a tomar x culo .
+
         fetch('http://localhost:6969/users').then((response) => {
             return response.json()
         }).then((json) => {
