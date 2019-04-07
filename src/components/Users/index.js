c106c6699d622d0c96d6e67f17052f4c5b6ab8b1
Pascual <pascual.munoz@pccomponentes.com>
2019-04-07 17:03:59 +0200

    with rxjs.

--- src/components/Users/index.js
+++ src/components/Users/index.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import GenericTable from '../GenericTable'
 import { ajax } from 'rxjs/ajax'
-
-console.log('ajax', ajax)//TODO: borrame.
-
+import {map, filter } from 'rxjs/operators'
+import {of } from 'rxjs'
 export default class Users extends React.Component {
     constructor (props) {
         super(props)
@@ -19,28 +18,39 @@ export default class Users extends React.Component {
         // de ahi el dolar del final
         // el flujo es de [ 1json ] al igual que el de un array de [1] elemento.
         const usersJson$ = ajax.getJSON('http://localhost:6969/users')
+        const GroupsJson$ = ajax.getJSON('http://localhost:6969/groups')
 
+        const observerDeUsers = {
+            next: (next) => {
+                    this.setState({
+                        isLoaded: true,
+                        users: next
+                    })
+            },
+            error: (error) => {},
+            complete: () => {},
+        }
         //nos subscribimos a ese flujo como observadores, para ello , le pasamos
         //3 callbacks ...
-        usersJson$.subscribe(
-            console.log, //onSuccess,
-            console.error, //onError
-            console.log('completado'), //onComplete , que este se usa poco xD
-        )
+        usersJson$.pipe(
+            map(function (item) {return item.users}),
+        ).subscribe(observerDeUsers)
+
+
         //y asincronia a tomar x culo .
 
-        fetch('http://localhost:6969/users').then((response) => {
-            return response.json()
-        }).then((json) => {
-            this.setState({
-                isLoaded: true,
-                users: json.users,
-            })
-        }).catch(error => {
-            this.setState({
-                error: 'error en el ajax, arranca el server',
-            })
-        })
+        // fetch('http://localhost:6969/users').then((response) => {
+        //     return response.json()
+        // }).then((json) => {
+        //     this.setState({
+        //         isLoaded: true,
+        //         users: json.users,
+        //     })
+        // }).catch(error => {
+        //     this.setState({
+        //         error: 'error en el ajax, arranca el server',
+        //     })
+        // })
 
     }
 
