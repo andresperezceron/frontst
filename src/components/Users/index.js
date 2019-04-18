605f89b7d811f31f6bbd3c63ee90a9643219fe40
andresperezceron <aperezlevi@gmail.com>
2019-04-18 10:51:32 +0200

    180419

--- src/components/Users/index.js
+++ src/components/Users/index.js
@@ -2,14 +2,14 @@ import React from 'react'
 import GenericTable from '../GenericTable'
 import { ajax } from 'rxjs/ajax'
 import {map, filter } from 'rxjs/operators'
-import {of } from 'rxjs'
+
 export default class Users extends React.Component {
     constructor (props) {
-        super(props)
+        super(props);
         this.state = {
             error: null,
             isLoaded: false,
-            users: [],
+            users: []
         }
     }
 
@@ -17,8 +17,8 @@ export default class Users extends React.Component {
         //creamos un observable del json que tira el endpoint,
         // de ahi el dolar del final
         // el flujo es de [ 1json ] al igual que el de un array de [1] elemento.
-        const usersJson$ = ajax.getJSON('http://localhost:6969/users')
-        const GroupsJson$ = ajax.getJSON('http://localhost:6969/groups')
+        const usersJson$ = ajax.getJSON('http://localhost:6969/users');
+        const GroupsJson$ = ajax.getJSON('http://localhost:6969/groups');
 
         const observerDeUsers = {
             next: (next) => {
@@ -28,12 +28,12 @@ export default class Users extends React.Component {
                     })
             },
             error: (error) => {},
-            complete: () => {},
-        }
+            complete: () => {}
+        };
         //nos subscribimos a ese flujo como observadores, para ello , le pasamos
         //3 callbacks ...
         usersJson$.pipe(
-            map(function (item) {return item.users}),
+            map(function(item) {return item.users})
         ).subscribe(observerDeUsers)
 
 
@@ -54,16 +54,14 @@ export default class Users extends React.Component {
 
     }
 
-    render () {
-        const { error, isLoaded, users } = this.state
+    render() {
+        const { error, isLoaded, users } = this.state;
         if (error) {
             return <div>Error: {error}</div>
         } else if (!isLoaded) {
             return <div>Loading...</div>
         } else {
-            return (
-                <GenericTable arrObj={users}/>
-            )
+            return(<GenericTable arrObj={users}/>)
         }
     }
 }
