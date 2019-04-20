e7fe88d4340cf6b75b93617793dc51bf6e55295e
andresperezceron <aperezlevi@gmail.com>
2019-04-20 21:52:24 +0200

    formulario inicial

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Users from './components/Users';
+import FormLogin from './components/FormLogin';
 
 const app = document.getElementById("root");
-ReactDOM.render(<Users/>, app);
+ReactDOM.render(<FormLogin/>, app);
