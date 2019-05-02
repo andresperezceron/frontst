c7eea31e8f99c4becb94b6d72582b847b2f86980
andresperezceron <aperezlevi@gmail.com>
2019-05-02 09:51:12 +0200

    style

--- src/components/FormLogin/constants.js
+++ src/components/FormLogin/constants.js
@@ -1,4 +1,4 @@
 export default Object.freeze({
-    NAME: 'name',
+    EMAIL: 'email',
     PASSWORD: 'password'
 })
--- src/components/FormLogin/index.jsx
+++ src/components/FormLogin/index.jsx
@@ -4,46 +4,64 @@ import constants from './constants'
 import {ajax} from "rxjs/ajax";
 
 
-const StyledForm = styled.form`
+const Form = styled.form`
   margin:  60px auto;
   padding: 12px 20px;
   width: 300px;
-  border: 1px solid grey;
+  border: 1px solid #ccc;
   border-radius: 5px;
   background: bisque;
 `;
 
+const Input = styled.input`
+  background-color: lightyellow;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  display: inline-block;
+  font-size: 14px;
+  margin: 8px 0;
+  padding: 12px 20px;
+  width: 100%;
+  :focus {border: 2px solid darkorange;}
+`;
+
 const Button = styled.button`
   width: 100%;
-  background-color: coral;
-  color: white;
+  background-color: darkorange;
+  color: lightyellow;
   padding: 14px 20px;
   margin: 8px 0;
   border: none;
   border-radius: 4px;
   cursor: pointer;
+  :hover {background-color: coral;}
+`;
+
+const A = styled.a`
+  text-align: center;
+  font-size: 15px;
+  color: darkorange;
+  display: block;
+  margin-top: 12px;
+  text-decoration: none;
+`;
+
+const P = styled.p`
+  color: saddlebrown;
+  text-align: center;
 `;
 
 const H1 = styled.h1`
   text-align: center;
-  color: #777;
-`;
-
-const StyledTextField = styled.input`
-  width: 100%;;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
+  color: darkorange;
 `;
 
 export default class FormLogin extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: undefined,
+            email: undefined,
             password: undefined,
             isLogin: true
         };
@@ -53,7 +71,7 @@ export default class FormLogin extends React.Component {
 
     handleChange(event) {
         switch(event.target.getAttribute("name")) {
-            case constants.NAME: this.setState( {name: event.target.value} ); break;
+            case constants.EMAIL: this.setState( {email: event.target.value} ); break;
             case constants.PASSWORD: this.setState( {password: event.target.value} ); break;
         }
     }
@@ -62,7 +80,7 @@ export default class FormLogin extends React.Component {
         const loginPost$ = ajax.post(
             'http://localhost:6969/login',
             {
-                name: this.state.name,
+                email: this.state.email,
                 password: this.state.password },
             {}
             );
@@ -79,13 +97,14 @@ export default class FormLogin extends React.Component {
     render() {
         const {isLogin} = this.state;
         return (
-            <StyledForm onSubmit={this.handleSubmit}>
-                <H1>Login</H1>
-                <StyledTextField name={constants.NAME} type="text" placeholder={"email"} onChange={this.handleChange}/>
-                <StyledTextField name={constants.PASSWORD} type="text" placeholder={"password"} onChange={this.handleChange}/>
-                {!isLogin && <p>email o contraseña incorrecto¡</p>}
+            <Form onSubmit={this.handleSubmit}>
+                <H1>Log in</H1>
+                <Input name={constants.EMAIL} type="text" placeholder={"email"} onChange={this.handleChange}/>
+                <Input name={constants.PASSWORD} type="password" placeholder={"password"} onChange={this.handleChange}/>
                 <Button>Enviar</Button>
-            </StyledForm>
+                {!isLogin && <P>Error: email o password incorrectos</P>}
+                <A href={""}>¿Olvidaste tu contraseña?</A>
+            </Form>
         );
     }
 }
