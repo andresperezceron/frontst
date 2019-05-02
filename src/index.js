1a4bb719ea10874c681eec539ebf97854a9e5c5f
Pascual <pascual.munoz@pccomponentes.com>
2019-05-02 12:32:58 +0200

    basic react router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Users from './components/Users';
-import FormLogin from './components/FormLogin';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import FormLogin from './components/FormLogin'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
-const app = document.getElementById("root");
-ReactDOM.render(<FormLogin/>, app);
+const app = document.getElementById('root')
+ReactDOM.render(
+    <Router>
+        <Link to={'/login'}>Login</Link>
+        <Route path={'/login'} component={FormLogin}/>
+    </Router>,
+    app)
